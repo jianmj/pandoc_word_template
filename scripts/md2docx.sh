fe35338b1ea674df93d77722f07f34fe89abf92b
Tron <achuan-2@outlook.com>
2026-01-22 10:20:03 +0800

    📝

--- scripts/md2docx.sh
+++ scripts/md2docx.sh
@@ -1,4 +1,4 @@
-pandoc input.md -t html | \
-    pandoc -f html -o output.docx \
+pandoc README.md -t html | \
+    pandoc -f html -o README.docx \
     --reference-doc template_标题不编号-列表第二行顶格.docx \
     --lua-filter markdown-to-docx.lua
